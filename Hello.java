baae3066e4cf02d749759961a6eafe470da60788
KMSAAR <aselarohana0522@gmail.com>
2023-09-13 09:40:29 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+// Display the String data type values
+public class Hello{
+  public static void main(String [] args){
+    System.out.println("Hello World!");
+    System.out.print("ITUM DIYAGAMA,\nDiyagama,\nHomagama.\n");
+    System.out.println("Sri lanaka");
+
+  }
+}
